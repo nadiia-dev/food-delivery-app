15fd9e8f55ecf8da78dbe95bbaa5905968743796
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-11 17:37:36 +0200

    init project && add first component

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import logo from "../assets/logo.jpg";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-[3rem] px-[10%]">
+      <div className="flex items-center gap-[1rem]">
+        <img
+          src={logo}
+          alt="A restaurant"
+          className="w-[4rem] h-[4rem] object-contain rounded-full border-2 boreder-yellow-400"
+        />
+        <h1>React Food</h1>
+      </div>
+      <nav>
+        <button>Cart</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr(), tsconfigPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      src: "/src",
+      assets: "/src/assets",
+    },
+  },
+  base: "/",
+});
