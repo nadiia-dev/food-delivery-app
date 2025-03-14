69c20573ea8a5e71bd9efbbb188626c9db0888c1
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-14 18:25:23 +0200

    save cart to localstorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userProgressReducer from "./userProgressSlice";
 import cartReducer from "./cartSlice";
+import { PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const persistConfig: PersistConfig<any> = {
+  key: "cart",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 
 const store = configureStore({
   reducer: {
     userProgress: userProgressReducer,
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
